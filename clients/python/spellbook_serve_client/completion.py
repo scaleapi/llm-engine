8c369e2b20e92b0a1efbc7fbddc5e86a27d2d94f
Yi Xu <yixu34@gmail.com>
2023-07-14 13:10:49 -0700

    Hackathon refresh docs (#19)
    * Add nav for guides
    
    * Add token streaming guide
    
    * Reorganize Completion examples so that they're grouped by acreate and create
    
    * Add fine-tuning examples

--- clients/python/spellbook_serve_client/completion.py
+++ clients/python/spellbook_serve_client/completion.py
@@ -11,51 +11,9 @@ from spellbook_serve_client.data_types import (
 
 class Completion(APIEngine):
     """
-    Completion API. This API is used to generate text completions.
-
-    Example:
-        ```python
-        from spellbook_serve_client import Completion
-
-        response = Completion.create(
-            model_name="llama-7b",
-            prompt="Hello, my name is",
-            max_new_tokens=10,
-            temperature=0.2,
-        )
-        print(response.outputs[0].text)
-        ```
-
-    Example:
-        ```python
-        from spellbook_serve_client import Completion
-
-        response_stream = Completion.create(
-            model_name="llama-7b",
-            prompt="Hello, my name is",
-            max_new_tokens=10,
-            temperature=0.2,
-            stream=True,
-        )
-        for response in response_stream:
-            print(response.output.text)
-        ```
-
-    Example:
-        ```python
-        from spellbook_serve_client import Completion
-
-        async def main():
-            response_stream = await Completion.acreate(
-                model_name="llama-7b",
-                prompt="Hello, my name is",
-                max_new_tokens=10,
-                temperature=0.2,
-                stream=True,
-            )
-            async for response in response_stream:
-                print(response.output.text)
-        ```
+    Completion API. This API is used to generate text completions. The Completions API can be run either
+    synchronous or asynchronously (via Python `asyncio`); for each of these modes, you can also choose to
+    stream token responses or not.
     """
 
     @classmethod
@@ -69,7 +27,27 @@ class Completion(APIEngine):
         stream: bool = False,
     ) -> Union[CompletionSyncV1Response, AsyncIterable[CompletionStreamV1Response]]:
         """
-        Create a completion task.
+        Create a completion task asynchronously.
+
+        Example without token streaming:
+            ```python
+            from spellbook_serve_client import Completion
+
+            async def main():
+                response_stream = await Completion.acreate(
+                    model_name="llama-7b",
+                    prompt="Hello, my name is",
+                    max_new_tokens=10,
+                    temperature=0.2,
+                )
+                async for response in response_stream:
+                    print(response.output.text)
+            ```
+
+        JSON Response:
+        ```json
+        ```
+
 
         Args:
             model_name (str):
@@ -87,7 +65,7 @@ class Completion(APIEngine):
                 `Iterator[CompletionStreamV1Response]`.
 
         Returns:
-            response (CompletionStreamV1Response): generated response or iterator of response chunks
+            response (Union[CompletionSyncV1Response, AsyncIterable[CompletionStreamV1Response]]): generated response or iterator of response chunks
         """
         if stream:
 
@@ -135,7 +113,63 @@ class Completion(APIEngine):
         stream: bool = False,
     ) -> Union[CompletionSyncV1Response, Iterator[CompletionStreamV1Response]]:
         """
-        Create a completion task.
+        Create a completion task synchronously.
+
+        Example request without token streaming:
+            ```python
+            from spellbook_serve_client import Completion
+
+            response = Completion.create(
+                model_name="llama-7b",
+                prompt="Hello, my name is",
+                max_new_tokens=10,
+                temperature=0.2,
+            )
+            print(response)
+            ```
+
+        JSON Response:
+            ```json
+            {
+                "status": "SUCCESS",
+                "outputs": [
+                    {
+                        "text": "\\nThe sky is blue because of the way the light is reflected off the molecules in the air.\\nWhat is the sky blue?\\nThe sky is blue because of the way the light is reflected off the molecules in the air.\\nWhat is the sky blue?\\nThe sky is blue because of the way the light is reflected off the molecules in the air. The sky is blue because of the way the light is reflected off the molecules in the air.\\nWhat is",
+                        "num_prompt_tokens": null,
+                        "num_completion_tokens": 100
+                    }
+                ],
+                "traceback": null
+            }
+            ```
+
+        Example request with token streaming:
+            ```python
+            from spellbook_serve_client import Completion
+
+            stream = Completion.create(
+                model_name="llama-7b",
+                prompt="why is the sky blue?",
+                max_new_tokens=5,
+                temperature=0.2,
+                stream=True,
+            )
+    
+            for response in stream:
+                if response.output:
+                    print(response.output)
+            ```
+
+        Stream response JSONs:
+            ```json
+            [
+                {"text": "\\n", "finished": false, "num_prompt_tokens": null, "num_completion_tokens": 1},
+                {"text": "I", "finished": false, "num_prompt_tokens": null, "num_completion_tokens": 2},
+                {"text": "'", "finished": false, "num_prompt_tokens": null, "num_completion_tokens": 3},
+                {"text": "m", "finished": false, "num_prompt_tokens": null, "num_completion_tokens": 4},
+                {"text": " not", "finished": true, "num_prompt_tokens": null, "num_completion_tokens": 5}
+            ]
+            ```
 
         Args:
             model_name (str):
@@ -152,7 +186,7 @@ class Completion(APIEngine):
                 Whether to stream the response. If true, the return type is an `Iterator`.
 
         Returns:
-            response (CompletionStreamV1Response): generated response or iterator of response chunks
+            response (Union[CompletionSyncV1Response, Iterator[CompletionStreamV1Response]]): generated response or iterator of response chunks
         """
         if stream:
 
---
+++ docs/guides/completions.md
@@ -0,0 +1,3 @@
+Given a prompt, the model will return one or more predicted Completions. The Completions API can be run either
+synchronous or asynchronously (via Python `asyncio`); for each of these modes, you can also choose to stream token
+responses or not.
