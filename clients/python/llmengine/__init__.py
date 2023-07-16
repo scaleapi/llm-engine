898e66e71076d2a9bcc4812f2acbdb550ad80cc3
Phil Chen <92065453+phil-scale@users.noreply.github.com>
2023-07-16 12:04:36 +0200

    bump version to 0.0.0.beta0 (#37)

--- clients/python/llmengine/__init__.py
+++ clients/python/llmengine/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.0.0.alpha1"
+__version__ = "0.0.0.beta0"
 
 from llmengine.completion import Completion
 from llmengine.data_types import (
