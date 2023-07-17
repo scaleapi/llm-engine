9df88b36c82759fd8ab0f5e38c02ddd5a45558dc
Phil Chen <phil.chen@scale.com>
2023-07-17 15:17:06 -0700

    0.0.0.beta2

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="scale-llm-engine",
     python_requires=">=3.7",
-    version="0.0.0.beta1",
+    version="0.0.0.beta2",
     packages=find_packages(),
 )
