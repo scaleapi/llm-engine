59bb6b784c06c02fb91edb1c2efbe9ca7aac331e
Yunfeng Bai <83252681+yunfeng-scale@users.noreply.github.com>
2023-07-19 09:35:51 -0700

    Add back Model.create (#125)
    * Add back Model.create
    
    * format

--- clients/python/llmengine/data_types.py
+++ clients/python/llmengine/data_types.py
@@ -113,6 +113,18 @@ class GetModelEndpointResponse(BaseModel):
     public_inference: Optional[bool] = Field(default=None)
 
 
+class PostInferenceHooks(str, Enum):
+    """
+    Post-inference hooks are functions that are called after inference is complete.
+
+    Attributes:
+        CALLBACK: The callback hook is called with the inference response and the task ID.
+    """
+
+    # INSIGHT = "insight"
+    CALLBACK: str = "callback"
+
+
 class CreateLLMEndpointRequest(BaseModel):
     name: str
 
