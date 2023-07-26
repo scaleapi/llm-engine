29078959aea78281a0068de6d5b4a154e9e4b08e
Katie Wu <katie.wu@scale.com>
2023-07-26 10:40:00 -0700

    Bump version to 0.0.0.beta7 (#178)

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="scale-llm-engine",
     python_requires=">=3.7",
-    version="0.0.0.beta6",
+    version="0.0.0.beta7",
     packages=find_packages(),
 )
