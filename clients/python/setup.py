24f6a3206218af7aec073d3e8801bbbc350406b9
Katie Wu <katie.wu@scale.com>
2023-08-01 16:28:27 -0700

    File API functions (#160)

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="scale-llm-engine",
     python_requires=">=3.7",
-    version="0.0.0.beta7",
+    version="0.0.0.beta8",
     packages=find_packages(),
 )
