d3e993d1790f642e5b2471906fdf4667cf4795ec
Ian Macleod <ian.macleod@scale.com>
2023-09-22 16:53:20 +0000

    adding timestamp to user logs for errors

--- model-engine/model_engine_server/api/app.py
+++ model-engine/model_engine_server/api/app.py
@@ -1,7 +1,9 @@
 import os
 import traceback
+from datetime import datetime
 from pathlib import Path
 
+import pytz
 from ddtrace import patch
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse
@@ -19,7 +21,7 @@ from model_engine_server.api.model_endpoints_docs_v1 import model_endpoints_docs
 from model_engine_server.api.model_endpoints_v1 import model_endpoint_router_v1
 from model_engine_server.api.tasks_v1 import inference_task_router_v1
 from model_engine_server.api.triggers_v1 import trigger_router_v1
-from model_engine_server.common.datadog_utils import get_request_id
+from model_engine_server.common.datadog_utils import add_trace_request_id, get_request_id
 from model_engine_server.core.loggers import filename_wo_ext, make_logger
 from starlette.middleware.base import BaseHTTPMiddleware
 
@@ -41,19 +43,28 @@ logger = make_logger(filename_wo_ext(__name__))
 
 
 class ExceptionLoggingMiddleware(BaseHTTPMiddleware):
+    def __init__(self):
+        self.timezone = pytz.timezone("US/Pacific")
+
     async def dispatch(self, request: Request, call_next):
         try:
             return await call_next(request)
         except Exception as e:
             tb_str = traceback.format_exception(etype=type(e), value=e, tb=e.__traceback__)
-            structured_log = {"error": str(e), "traceback": "".join(tb_str)}
-            logger.error("Unhandled exception: %s", structured_log)
             request_id = get_request_id()
+            timestamp = datetime.now(self.timezone).strftime("%Y-%m-%d %H:%M:%S %Z")
+            add_trace_request_id(request_id)
+            structured_log = {
+                "error": str(e),
+                "request_id": str(request_id),
+                "traceback": "".join(tb_str),
+            }
+            logger.error("Unhandled exception: %s", structured_log)
             return JSONResponse(
                 {
                     "status_code": 500,
                     "content": {
-                        "error": f"Internal error for request_id {request_id}. Our team has been notified."
+                        "error": f"Internal error for request_id {request_id} at time {timestamp}. Our team has been notified."
                     },
                 }
             )
