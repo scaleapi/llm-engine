6f7f4473aeed3cb7b3c3294aa2d7e893a27634ad
Ian Macleod <ian.macleod@scale.com>
2023-09-27 21:45:35 +0000

    bump the llm engine pypi version

--- clients/python/llmengine/__init__.py
+++ clients/python/llmengine/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.0.0.b14"
+__version__ = "0.0.0.b15"
 
 import os
 from typing import Sequence
