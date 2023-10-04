8e9f0c9d753740ef664c7e1383332e99b486eb35
Ian Macleod <139901935+ian-scale@users.noreply.github.com>
2023-10-03 17:51:44 -0700

    bump pypi version (#303)

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="scale-llm-engine",
     python_requires=">=3.7",
-    version="0.0.0.beta17",
+    version="0.0.0.beta18",
     packages=find_packages(),
 )
