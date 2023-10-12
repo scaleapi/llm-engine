d3f1cf820a9875b1a917e782041e30db5b1ceaba
Ian Macleod <ian.macleod@scale.com>
2023-10-12 00:56:31 +0000

    iterating

--- model-engine/model_engine_server/api/llms_v1.py
+++ model-engine/model_engine_server/api/llms_v1.py
@@ -31,7 +31,13 @@ from model_engine_server.common.dtos.llms import (
 )
 from model_engine_server.common.dtos.model_endpoints import ModelEndpointOrderBy
 from model_engine_server.core.auth.authentication_repository import User
-from model_engine_server.core.loggers import filename_wo_ext, get_request_id, make_logger
+from model_engine_server.core.loggers import (
+    filename_wo_ext,
+    get_request_id,
+    make_logger,
+    set_team_id,
+    set_user_id,
+)
 from model_engine_server.domain.exceptions import (
     EndpointDeleteFailedException,
     EndpointLabelsException,
@@ -81,6 +87,8 @@ async def create_model_endpoint(
     Creates an LLM endpoint for the current user.
     """
     add_trace_resource_name("llm_model_endpoints_post")
+    set_user_id(auth.user_id if auth else None)
+    set_team_id(auth.team_id if auth else None)
     logger.info(f"POST /llm/model-endpoints with {request} for {auth}")
     try:
         create_model_bundle_use_case = CreateModelBundleV2UseCase(
@@ -135,6 +143,8 @@ async def list_model_endpoints(
     Lists the LLM model endpoints owned by the current owner, plus all public_inference LLMs.
     """
     add_trace_resource_name("llm_model_endpoints_get")
+    set_user_id(auth.user_id if auth else None)
+    set_team_id(auth.team_id if auth else None)
     logger.info(f"GET /llm/model-endpoints?name={name}&order_by={order_by} for {auth}")
     use_case = ListLLMModelEndpointsV1UseCase(
         llm_model_endpoint_service=external_interfaces.llm_model_endpoint_service,
@@ -154,6 +164,8 @@ async def get_model_endpoint(
     Describe the LLM Model endpoint with given name.
     """
     add_trace_resource_name("llm_model_endpoints_name_get")
+    set_user_id(auth.user_id if auth else None)
+    set_team_id(auth.team_id if auth else None)
     logger.info(f"GET /llm/model-endpoints/{model_endpoint_name} for {auth}")
     try:
         use_case = GetLLMModelEndpointByNameV1UseCase(
@@ -178,6 +190,8 @@ async def create_completion_sync_task(
     Runs a sync prompt completion on an LLM.
     """
     add_trace_resource_name("llm_completion_sync_post")
+    set_user_id(auth.user_id if auth else None)
+    set_team_id(auth.team_id if auth else None)
     logger.info(
         f"POST /completion_sync with {request} to endpoint {model_endpoint_name} for {auth}"
     )
@@ -223,6 +237,8 @@ async def create_completion_stream_task(
     Runs a stream prompt completion on an LLM.
     """
     add_trace_resource_name("llm_completion_stream_post")
+    set_user_id(auth.user_id if auth else None)
+    set_team_id(auth.team_id if auth else None)
     logger.info(
         f"POST /completion_stream with {request} to endpoint {model_endpoint_name} for {auth}"
     )
@@ -271,6 +287,8 @@ async def create_fine_tune(
     external_interfaces: ExternalInterfaces = Depends(get_external_interfaces),
 ) -> CreateFineTuneResponse:
     add_trace_resource_name("fine_tunes_create")
+    set_user_id(auth.user_id if auth else None)
+    set_team_id(auth.team_id if auth else None)
     logger.info(f"POST /fine-tunes with {request} for {auth}")
     try:
         use_case = CreateFineTuneV1UseCase(
@@ -300,6 +318,8 @@ async def get_fine_tune(
     external_interfaces: ExternalInterfaces = Depends(get_external_interfaces_read_only),
 ) -> GetFineTuneResponse:
     add_trace_resource_name("fine_tunes_get")
+    set_user_id(auth.user_id if auth else None)
+    set_team_id(auth.team_id if auth else None)
     logger.info(f"GET /fine-tunes/{fine_tune_id} for {auth}")
     try:
         use_case = GetFineTuneV1UseCase(
@@ -319,6 +339,8 @@ async def list_fine_tunes(
     external_interfaces: ExternalInterfaces = Depends(get_external_interfaces_read_only),
 ) -> ListFineTunesResponse:
     add_trace_resource_name("fine_tunes_list")
+    set_user_id(auth.user_id if auth else None)
+    set_team_id(auth.team_id if auth else None)
     logger.info(f"GET /fine-tunes for {auth}")
     use_case = ListFineTunesV1UseCase(
         llm_fine_tuning_service=external_interfaces.llm_fine_tuning_service,
@@ -333,6 +355,8 @@ async def cancel_fine_tune(
     external_interfaces: ExternalInterfaces = Depends(get_external_interfaces),
 ) -> CancelFineTuneResponse:
     add_trace_resource_name("fine_tunes_cancel")
+    set_user_id(auth.user_id if auth else None)
+    set_team_id(auth.team_id if auth else None)
     logger.info(f"PUT /fine-tunes/{fine_tune_id}/cancel for {auth}")
     try:
         use_case = CancelFineTuneV1UseCase(
@@ -353,6 +377,8 @@ async def get_fine_tune_events(
     external_interfaces: ExternalInterfaces = Depends(get_external_interfaces_read_only),
 ) -> GetFineTuneEventsResponse:
     add_trace_resource_name("fine_tunes_events_get")
+    set_user_id(auth.user_id if auth else None)
+    set_team_id(auth.team_id if auth else None)
     logger.info(f"GET /fine-tunes/{fine_tune_id}/events for {auth}")
     try:
         use_case = GetFineTuneEventsV1UseCase(
@@ -374,6 +400,8 @@ async def download_model_endpoint(
     external_interfaces: ExternalInterfaces = Depends(get_external_interfaces),
 ) -> ModelDownloadResponse:
     add_trace_resource_name("model_endpoints_download")
+    set_user_id(auth.user_id if auth else None)
+    set_team_id(auth.team_id if auth else None)
     logger.info(f"POST /model-endpoints/download with {request} for {auth}")
     try:
         use_case = ModelDownloadV1UseCase(
@@ -398,6 +426,8 @@ async def delete_llm_model_endpoint(
     external_interfaces: ExternalInterfaces = Depends(get_external_interfaces),
 ) -> DeleteLLMEndpointResponse:
     add_trace_resource_name("llm_model_endpoints_delete")
+    set_user_id(auth.user_id if auth else None)
+    set_team_id(auth.team_id if auth else None)
     logger.info(f"DELETE /model-endpoints/{model_endpoint_name} for {auth}")
     try:
         use_case = DeleteLLMEndpointByNameUseCase(
