dd5bb56df9534e5f9665a928764e662767683bb0
Sai Atmakuri <sai.atmakuri@scale.com>
2023-10-19 21:22:48 +0000

    change profile

--- model-engine/model_engine_server/core/celery/app.py
+++ model-engine/model_engine_server/core/celery/app.py
@@ -504,7 +504,7 @@ def _get_backend_url_and_conf(
     elif backend_protocol == "s3":
         backend_url = "s3://"
         if aws_role is None:
-            aws_profile = os.getenv("AWS_PROFILE", infra_config().profile_ml_worker)
+            aws_profile = os.getenv("S3_WRITE_AWS_PROFILE", infra_config().profile_ml_worker)
             aws_session = session(aws_profile)
         else:
             aws_session = session(aws_role)
