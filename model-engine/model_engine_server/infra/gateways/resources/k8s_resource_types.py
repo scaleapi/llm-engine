5f7c5667b11a2aa803466e1d42d974760c0fa586
William Song <song-william@users.noreply.github.com>
2023-11-03 15:12:00 -0700

    Unmount aws config from root (#361)
    * unmount aws config from root
    
    * move mount out of /root
    
    * modify main_env in python rather than templates

--- model-engine/model_engine_server/infra/gateways/resources/k8s_resource_types.py
+++ model-engine/model_engine_server/infra/gateways/resources/k8s_resource_types.py
@@ -518,6 +518,8 @@ def get_endpoint_resource_arguments_from_request(
     if isinstance(flavor, RunnableImageLike) and flavor.env:
         main_env = [{"name": key, "value": value} for key, value in flavor.env.items()]
     main_env.append({"name": "AWS_PROFILE", "value": build_endpoint_request.aws_role})
+    # NOTE: /opt/.aws/config is where service_template_config_map.yaml mounts the AWS config file, point to the mount for boto clients
+    main_env.append({"name": "AWS_CONFIG_FILE", "value": "/opt/.aws/config"})
 
     infra_service_config_volume_mount_path = "/infra-config"
     forwarder_config_file_name = "service--forwarder.yaml"
