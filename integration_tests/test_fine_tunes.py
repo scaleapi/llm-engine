f71f441ace5727ca9d3e2ac1300e362698828568
Tiffany Zhao <tiffany.zhao@scale.com>
2023-11-13 17:12:16 +0000

    make test work

--- integration_tests/test_fine_tunes.py
+++ integration_tests/test_fine_tunes.py
@@ -1,31 +1,56 @@
-import pytest
+import json
+import os
 
-from .rest_api_utils import (  # CREATE_FINE_TUNE_DI_BATCH_JOB_BUNDLE_REQUEST, CREATE_FINE_TUNE_REQUEST, USER_ID_0, cancel_fine_tune_by_id, create_docker_image_batch_job_bundle, create_fine_tune, get_fine_tune_by_id,
+import boto3
+import smart_open
+
+from .rest_api_utils import (
+    CREATE_FINE_TUNE_DI_BATCH_JOB_BUNDLE_REQUEST,
+    CREATE_FINE_TUNE_REQUEST,
     USER_ID_0,
+    cancel_fine_tune_by_id,
+    create_fine_tune,
+    get_fine_tune_by_id,
+    get_or_create_docker_image_batch_job_bundle,
     list_fine_tunes,
 )
 
 
-@pytest.mark.skip(reason="test doesn't currently work, needs to be implemented correctly")
 def test_fine_tunes() -> None:
-    # TODO: get this test to work (move LLM fine tune repository to database rather than in S3)
+    di_batch_job_id = get_or_create_docker_image_batch_job_bundle(
+        CREATE_FINE_TUNE_DI_BATCH_JOB_BUNDLE_REQUEST, USER_ID_0
+    )["id"]
 
-    # di_batch_job_id = create_docker_image_batch_job_bundle(
-    #     CREATE_FINE_TUNE_DI_BATCH_JOB_BUNDLE_REQUEST, USER_ID_0
-    # )["docker_image_batch_job_bundle_id"]
+    data = {
+        "test_base_model-lora": {
+            "docker_image_batch_job_bundle_id": di_batch_job_id,
+            "launch_bundle_config": {},
+            "launch_endpoint_config": {},
+            "default_hparams": {},
+            "required_params": [],
+        }
+    }
 
-    # create_response = create_fine_tune(CREATE_FINE_TUNE_REQUEST, USER_ID_0)
-    # fine_tune_id = create_response["id"]
+    if os.getenv("CIRCLECI") == "true":
+        session = boto3.Session()
+        aws_s3_bucket = os.getenv("CIRCLECI_AWS_S3_BUCKET")
+        client = session.client("s3")
+        with smart_open.open(
+            f"s3://{aws_s3_bucket}/fine_tune_repository",
+            "w",
+            transport_params={"client": client},
+        ) as f:
+            json.dump(data, f)
 
-    # get_response = get_fine_tune_by_id(fine_tune_id, USER_ID_0)
-    # assert get_response["id"] == fine_tune_id
+    create_response = create_fine_tune(CREATE_FINE_TUNE_REQUEST, USER_ID_0)
+    fine_tune_id = create_response["id"]
+    get_response = get_fine_tune_by_id(fine_tune_id, USER_ID_0)
+    assert get_response["id"] == fine_tune_id
 
-    # list_response_0_before = list_fine_tunes(USER_ID_0)
-    # num_jobs = len(list_response_0_before["jobs"])
-    # assert num_jobs >= 1
+    list_response_0_before = list_fine_tunes(USER_ID_0)
+    num_jobs = len(list_response_0_before["jobs"])
+    assert num_jobs >= 1
 
-    list_response_1 = list_fine_tunes(USER_ID_0)
-    assert len(list_response_1["jobs"]) == 0
-
-    # list_response_0_after = list_fine_tunes(USER_ID_0)
-    # assert len(list_response_0_after["jobs"]) == num_jobs - 1
+    cancel_fine_tune_by_id(fine_tune_id, USER_ID_0)
+    list_response_0_after = list_fine_tunes(USER_ID_0)
+    assert len(list_response_0_after["jobs"]) == num_jobs - 1
