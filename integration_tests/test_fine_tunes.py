998ce6010ac689a5230cb22fa1aa52c4927befd9
Tiffany Zhao <tiffany.zhao@scale.com>
2023-11-13 19:32:03 +0000

    add status checking

--- integration_tests/test_fine_tunes.py
+++ integration_tests/test_fine_tunes.py
@@ -2,7 +2,9 @@ import json
 import os
 
 import boto3
+import pytest
 import smart_open
+from fastapi import HTTPException
 
 from .rest_api_utils import (
     CREATE_FINE_TUNE_DI_BATCH_JOB_BUNDLE_REQUEST,
@@ -42,15 +44,24 @@ def test_fine_tunes() -> None:
         ) as f:
             json.dump(data, f)
 
-    create_response = create_fine_tune(CREATE_FINE_TUNE_REQUEST, USER_ID_0)
+    with pytest.raises(HTTPException) as e:
+        create_response = create_fine_tune(CREATE_FINE_TUNE_REQUEST, USER_ID_0)
+    assert e.type is None
+
     fine_tune_id = create_response["id"]
     get_response = get_fine_tune_by_id(fine_tune_id, USER_ID_0)
+    while get_response["status"] not in ["SUCCESS", "FAILURE"]:
+        get_response = get_fine_tune_by_id(fine_tune_id, USER_ID_0)
     assert get_response["id"] == fine_tune_id
+    assert get_response["status"] == "SUCCESS"
 
     list_response_0_before = list_fine_tunes(USER_ID_0)
     num_jobs = len(list_response_0_before["jobs"])
     assert num_jobs >= 1
 
-    cancel_fine_tune_by_id(fine_tune_id, USER_ID_0)
+    with pytest.raises(HTTPException) as e:
+        cancel_fine_tune_by_id(fine_tune_id, USER_ID_0)
+    assert e.type is None
+
     list_response_0_after = list_fine_tunes(USER_ID_0)
     assert len(list_response_0_after["jobs"]) == num_jobs - 1
