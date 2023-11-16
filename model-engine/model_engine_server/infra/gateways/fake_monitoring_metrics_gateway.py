34728ad645b92d9a5d00cf5f958a057dfe1f95cc
Sai Atmakuri <sai.atmakuri@scale.com>
2023-11-16 01:10:31 +0000

    emit metrics on token counts

--- model-engine/model_engine_server/infra/gateways/fake_monitoring_metrics_gateway.py
+++ model-engine/model_engine_server/infra/gateways/fake_monitoring_metrics_gateway.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 
+from model_engine_server.common.dtos.llms import TokenUsage
 from model_engine_server.domain.gateways.monitoring_metrics_gateway import (
     MetricMetadata,
     MonitoringMetricsGateway,
@@ -19,6 +20,7 @@ class FakeMonitoringMetricsGateway(MonitoringMetricsGateway):
         self.database_cache_hit = 0
         self.database_cache_miss = 0
         self.route_call = defaultdict(int)
+        self.token_count = 0
 
     def reset(self):
         self.attempted_build = 0
@@ -32,6 +34,7 @@ class FakeMonitoringMetricsGateway(MonitoringMetricsGateway):
         self.database_cache_hit = 0
         self.database_cache_miss = 0
         self.route_call = defaultdict(int)
+        self.token_count = 0
 
     def emit_attempted_build_metric(self):
         self.attempted_build += 1
@@ -65,3 +68,7 @@ class FakeMonitoringMetricsGateway(MonitoringMetricsGateway):
 
     def emit_route_call_metric(self, route: str, _metadata: MetricMetadata):
         self.route_call[route] += 1
+        print("route_call", route, self.route_call[route])
+
+    def emit_token_count_metrics(self, token_usage: TokenUsage, _metadata: MetricMetadata):
+        self.token_count += token_usage.num_total_tokens
