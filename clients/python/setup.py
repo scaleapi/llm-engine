5db6e08907a85df1d825c7e7a675a3f89bf27bea
Ian Macleod <ian.macleod@scale.com>
2023-11-29 18:51:57 +0000

    fixing enum value

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="scale-llm-engine",
     python_requires=">=3.7",
-    version="0.0.0.beta20",
+    version="0.0.0.beta21",
     packages=find_packages(),
 )
