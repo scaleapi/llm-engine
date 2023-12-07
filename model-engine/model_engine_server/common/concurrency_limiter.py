69e07ff29f8c414e480d91e6ca3ad763ae7866ba
Katie Wu <katie.wu@scale.com>
2023-12-06 18:35:25 -0800

    Add MultiprocessingConcurrencyLimiter to gateway (#399)

---
+++ model-engine/model_engine_server/common/concurrency_limiter.py
@@ -0,0 +1,36 @@
+from multiprocessing import BoundedSemaphore
+from multiprocessing.synchronize import BoundedSemaphore as BoundedSemaphoreType
+from typing import Optional
+
+from fastapi import HTTPException
+from model_engine_server.core.loggers import logger_name, make_logger
+
+logger = make_logger(logger_name())
+
+
+class MultiprocessingConcurrencyLimiter:
+    def __init__(self, concurrency: Optional[int], fail_on_concurrency_limit: bool):
+        self.concurrency = concurrency
+        if concurrency is not None:
+            if concurrency < 1:
+                raise ValueError("Concurrency should be at least 1")
+            self.semaphore: Optional[BoundedSemaphoreType] = BoundedSemaphore(value=concurrency)
+            self.blocking = (
+                not fail_on_concurrency_limit
+            )  # we want to block if we want to queue up requests
+        else:
+            self.semaphore = None
+            self.blocking = False  # Unused
+
+    def __enter__(self):
+        logger.debug("Entering concurrency limiter semaphore")
+        if self.semaphore and not self.semaphore.acquire(block=self.blocking):
+            logger.warning(f"Too many requests (max {self.concurrency}), returning 429")
+            raise HTTPException(status_code=429, detail="Too many requests")
+            # Just raises an HTTPException.
+            # __exit__ should not run; otherwise the release() doesn't have an acquire()
+
+    def __exit__(self, type, value, traceback):
+        logger.debug("Exiting concurrency limiter semaphore")
+        if self.semaphore:
+            self.semaphore.release()
