2fdf23d6e6d3dc247e6baac8e0255cd06990caf8
Ian Macleod <139901935+ian-scale@users.noreply.github.com>
2023-12-07 10:58:21 -0800

    change code-llama to codellama (#400)
    * change code-llama to codellama
    
    * use both code-llama and codellama temporarily

--- model-engine/model_engine_server/domain/use_cases/llm_model_endpoint_use_cases.py
+++ model-engine/model_engine_server/domain/use_cases/llm_model_endpoint_use_cases.py
@@ -190,6 +190,10 @@ _VLLM_MODEL_LENGTH_OVERRIDES: Dict[str, Dict[str, Optional[int]]] = {
     # Based on config here: https://huggingface.co/TIGER-Lab/MAmmoTH-Coder-7B/blob/main/config.json#L12
     # Can also see 13B, 34B there too
     "code-llama": {"max_model_len": 16384, "max_num_batched_tokens": 16384},
+    "codellama": {
+        "max_model_len": 16384,
+        "max_num_batched_tokens": 16384,
+    },  # setting both for backwards compatibility, will phase code-llama out in a future pr
     # Based on config here: https://huggingface.co/codellama/CodeLlama-7b-hf/blob/main/config.json#L12
     # Can also see 13B, 34B there too
     "llama-2": {"max_model_len": None, "max_num_batched_tokens": 4096},
