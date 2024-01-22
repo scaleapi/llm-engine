ae4965b8cc7777cc81d8bff77ce407aa643d1775
Katie Wu <katie.wu@scale.com>
2024-01-22 21:37:50 +0000

    service builder celery app

--- model-engine/model_engine_server/service_builder/celery.py
+++ model-engine/model_engine_server/service_builder/celery.py
@@ -1,3 +1,4 @@
+from model_engine_server.common.dtos.model_endpoints import BrokerType
 from model_engine_server.core.celery import celery_app
 from model_engine_server.core.config import infra_config
 
@@ -7,6 +8,10 @@ service_builder_service = celery_app(
         "model_engine_server.service_builder.tasks_v1",
     ],
     s3_bucket=infra_config().s3_bucket,
+    broker_type=str(BrokerType.SERVICEBUS.value)
+    if infra_config().cloud_provider == "azure"
+    else str(BrokerType.REDIS.value),
+    backend_protocol="abs" if infra_config().cloud_provider == "azure" else "s3",
 )
 
 if __name__ == "__main__":
