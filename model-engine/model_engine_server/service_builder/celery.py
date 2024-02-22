6a13ab9f6abe3e7aaf7eab2562c7a8b0b79a565b
Sai Atmakuri <sai.atmakuri@scale.com>
2024-02-22 04:26:53 +0000

    service builder uses redis for circleci

--- model-engine/model_engine_server/service_builder/celery.py
+++ model-engine/model_engine_server/service_builder/celery.py
@@ -1,6 +1,15 @@
 from model_engine_server.common.dtos.model_endpoints import BrokerType
+from model_engine_server.common.env_vars import CIRCLECI
 from model_engine_server.core.celery import celery_app
 from model_engine_server.core.config import infra_config
+
+service_builder_broker_type: str
+if CIRCLECI:
+    service_builder_broker_type = str(BrokerType.REDIS.value)
+elif infra_config().cloud_provider == "azure":
+    service_builder_broker_type = str(BrokerType.SERVICEBUS.value)
+else:
+    service_builder_broker_type = str(BrokerType.SQS.value)
 
 service_builder_service = celery_app(
     name="model_engine_server.service_builder",
@@ -8,11 +17,7 @@ service_builder_service = celery_app(
         "model_engine_server.service_builder.tasks_v1",
     ],
     s3_bucket=infra_config().s3_bucket,
-    broker_type=(
-        str(BrokerType.SERVICEBUS.value)
-        if infra_config().cloud_provider == "azure"
-        else str(BrokerType.SQS.value)
-    ),
+    broker_type=service_builder_broker_type,
     backend_protocol="abs" if infra_config().cloud_provider == "azure" else "s3",
 )
 
