7a2e85b0a7d3130e5a97e01ea327935dfb360013
Katie Wu <katie.wu@scale.com>
2024-03-15 19:04:13 +0000

    coverage

--- model-engine/model_engine_server/inference/infra/gateways/datadog_inference_monitoring_metrics_gateway.py
+++ model-engine/model_engine_server/inference/infra/gateways/datadog_inference_monitoring_metrics_gateway.py
@@ -14,7 +14,7 @@ class DatadogInferenceMonitoringMetricsGateway(InferenceMonitoringMetricsGateway
     def emit_async_task_received_metric(self, queue_name: str):
         statsd.increment(
             "scale_launch.async_task.received.count", tags=[f"queue_name:{queue_name}"]
-        )
+        )  # pragma: no cover
 
     def emit_async_task_stuck_metric(self, queue_name: str):
         statsd.increment("scale_launch.async_task.stuck.count", tags=[f"queue_name:{queue_name}"])
