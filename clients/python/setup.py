d0d6b8bbaef955cf4f3dc8e0d7a5e7aa9e33c38b
Katie Wu <katie.wu@scale.com>
2024-05-02 15:08:29 -0700

    Default include_stop_str_in_output to None (#506)

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="scale-llm-engine",
     python_requires=">=3.7",
-    version="0.0.0.beta30",
+    version="0.0.0.beta31",
     packages=find_packages(),
 )
