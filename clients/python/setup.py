1595df7de32c1237461e742562541ffc957b44ac
Michael Choi <michael.choi@scale.com>
2024-05-07 17:58:32 +0000

    Fix package name mapping

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -5,5 +5,5 @@ setup(
     python_requires=">=3.7",
     version="0.0.0.beta32",
     packages=find_packages(),
-    package_data={"scale_llm_engine": ["py.typed"]},
+    package_data={"llmengine": ["py.typed"]},
 )
