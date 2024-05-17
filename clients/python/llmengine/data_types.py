6cac2db9b32a95b5d828ee468a2aa3ad98bcdae8
Yunfeng Bai <83252681+yunfeng-scale@users.noreply.github.com>
2024-05-17 15:10:18 -0700

    Allow H100 to be used (#522)
    * Allow H100 to be used
    
    * Add MIG groups

--- clients/python/llmengine/data_types.py
+++ clients/python/llmengine/data_types.py
@@ -41,6 +41,9 @@ class GpuType(str, Enum):
     NVIDIA_AMPERE_A10 = "nvidia-ampere-a10"
     NVIDIA_AMPERE_A100 = "nvidia-ampere-a100"
     NVIDIA_AMPERE_A100E = "nvidia-ampere-a100e"
+    NVIDIA_HOPPER_H100 = "nvidia-hopper-h100"
+    NVIDIA_HOPPER_H100_1G_20GB = "nvidia-hopper-h100-1g20gb"
+    NVIDIA_HOPPER_H100_3G_40GB = "nvidia-hopper-h100-3g40gb"
 
 
 class ModelEndpointType(str, Enum):
