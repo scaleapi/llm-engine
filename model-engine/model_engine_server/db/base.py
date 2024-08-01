22fb208276313e5ebe4ab1fb29ee20af54009f03
Michael Choi <michael.choi@scale.com>
2024-08-01 17:28:49 +0000

    Create DBManager to better encapsulate connection pooling + refreshing

--- model-engine/model_engine_server/db/base.py
+++ model-engine/model_engine_server/db/base.py
@@ -2,24 +2,27 @@ import asyncio
 import os
 import sys
 import time
+from dataclasses import dataclass
 from typing import Iterator, Optional
 
 import sqlalchemy
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from model_engine_server.core.aws.secrets import get_key_file
-from model_engine_server.core.config import infra_config
+from model_engine_server.core.config import InfraConfig, infra_config
 from model_engine_server.core.loggers import logger_name, make_logger
-from sqlalchemy import create_engine
-from sqlalchemy.ext.asyncio import async_scoped_session, async_sessionmaker, create_async_engine
+from sqlalchemy import Engine, create_engine
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    async_scoped_session,
+    async_sessionmaker,
+    create_async_engine,
+)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
 
 logger = make_logger(logger_name())
-
-database_credential_expiration_timestamp = time.time()
-EXPIRATION_BUFFER = 300  # 5 minutes
 
 
 def get_key_file_name(environment: str) -> str:
@@ -28,13 +31,19 @@ def get_key_file_name(environment: str) -> str:
     return f"{environment}/ml_infra_pg".replace("training", "prod").replace("-new", "")
 
 
+@dataclass
+class DBConnection:
+    url: str
+    expiry_in_sec: Optional[int] = None
+
+
 def get_engine_url(
     env: Optional[str] = None,
     read_only: bool = True,
     sync: bool = True,
-    reset_expiration_timestamp: bool = False,
-) -> str:
+) -> DBConnection:
     """Gets the URL of the Postgresql engine depending on the environment."""
+    expiry_in_sec: Optional[int] = None
     if os.getenv("ML_INFRA_DATABASE_URL"):
         # In CircleCI environment, we set up a test in another container and specify the URL.
         engine_url = os.getenv("ML_INFRA_DATABASE_URL")
@@ -67,12 +76,10 @@ def get_engine_url(
                 "https://ossrdbms-aad.database.windows.net/.default"
             )
             password = token.token
-            if reset_expiration_timestamp:
-                global database_credential_expiration_timestamp
-                database_credential_expiration_timestamp = token.expires_on
             logger.info(f"Connecting to db {db} as user {user}")
 
             engine_url = f"postgresql://{user}:{password}@{db}?sslmode=require"
+            expiry_in_sec = token.expires_on
         else:
             db_secret_aws_profile = os.environ.get("DB_SECRET_AWS_PROFILE")
             creds = get_key_file(key_file, db_secret_aws_profile)
@@ -93,177 +100,208 @@ def get_engine_url(
         engine_url = engine_url.replace("postgresql://", "postgresql+asyncpg://").replace(
             "sslmode", "ssl"
         )
-    return engine_url
+    return DBConnection(engine_url, expiry_in_sec)
 
 
-# Synchronous sessions (Session and SessionReadOnly) are fairly straightforward, and both
-# can be used at any time. To use asynchronous sqlalchemy, use the SessionAsyncNullPool
-# if you're running a synchronous program where concurrency of database connections is not
-# super important (e.g. Celery workers that use long-standing connections, and Celery is currently
-# synchronous). Use SessionAsync and SessionReadOnlyAsync in ASGI applications.
-
-_Session: Optional[sessionmaker] = None
-_SessionReadOnly: Optional[sessionmaker] = None
-_SessionAsync: Optional[async_scoped_session] = None
-_SessionAsyncNullPool: Optional[async_scoped_session] = None
-_SessionReadOnlyAsync: Optional[async_scoped_session] = None
+@dataclass
+class SyncDBSession:
+    engine: Engine
+    session: sessionmaker
 
 
-def refresh_sessions():
-    # Try pool_pre_ping=True, see
-    # https://docs.sqlalchemy.org/en/14/core/engines.html
-    #   ?highlight=create_engine#sqlalchemy.create_engine.params.pool_pre_ping
-    # tl;dr is hopefully it stops the psycopg errors from happening
-    # Another probably less impactful (ie it shouldn't increase latency by as much,
-    # but also shouldn't get rid of as many errors e.g. 95% compared to 99.9%)
-    # option is to try pool_recycle = something kinda short e.g. a minute
-    # pool_pre_ping=True seems to not increase latency by very much
-    # (I profiled 2.7 ms -> 3.3 ms on GET model_bundles/)
-    # but hopefully should completely eliminate
-    # any of the postgres connection errors we've been seeing.
-    pool_pre_ping = infra_config().db_engine_disconnect_strategy == "pessimistic"
-    pool_size = infra_config().db_engine_pool_size
-    max_overflow = infra_config().db_engine_max_overflow
-    echo = infra_config().db_engine_echo
-    echo_pool = infra_config().db_engine_echo_pool
-
-    pg_engine = create_engine(
-        get_engine_url(read_only=False, sync=True, reset_expiration_timestamp=True),
-        echo=echo,
-        echo_pool=echo_pool,
-        pool_pre_ping=pool_pre_ping,
-        pool_size=pool_size,
-        max_overflow=max_overflow,
-        future=True,
-        logging_name="sync",
-    )
-    pg_engine_read_only = create_engine(
-        get_engine_url(read_only=True, sync=True),
-        echo=echo,
-        echo_pool=echo_pool,
-        pool_pre_ping=pool_pre_ping,
-        pool_size=pool_size,
-        max_overflow=max_overflow,
-        future=True,
-        logging_name="sync_ro",
-    )
-    pg_engine_async = create_async_engine(
-        get_engine_url(read_only=False, sync=False),
-        echo=echo,
-        echo_pool=echo_pool,
-        pool_pre_ping=pool_pre_ping,
-        pool_size=pool_size,
-        max_overflow=max_overflow,
-        future=True,
-        logging_name="async",
-    )
-    pg_engine_read_only_async = create_async_engine(
-        get_engine_url(read_only=True, sync=False),
-        echo=echo,
-        echo_pool=echo_pool,
-        pool_pre_ping=pool_pre_ping,
-        pool_size=pool_size,
-        max_overflow=max_overflow,
-        future=True,
-        logging_name="async_ro",
-    )
-    pg_engine_async_null_pool = create_async_engine(
-        get_engine_url(read_only=False, sync=False),
-        echo=echo,
-        echo_pool=echo_pool,
-        future=True,
-        poolclass=NullPool,
-        logging_name="async_null",
-    )
-
-    global _Session
-    global _SessionReadOnly
-    global _SessionAsync
-    global _SessionAsyncNullPool
-    global _SessionReadOnlyAsync
-
-    _Session = sessionmaker(autocommit=False, autoflush=False, bind=pg_engine)
-    _SessionReadOnly = sessionmaker(autocommit=False, autoflush=False, bind=pg_engine_read_only)
-    _SessionAsync = async_scoped_session(
-        session_factory=async_sessionmaker(
-            autocommit=False,
-            autoflush=False,
-            bind=pg_engine_async,
-            expire_on_commit=False,
-        ),
-        scopefunc=asyncio.current_task,
-    )
-    _SessionAsyncNullPool = async_scoped_session(
-        session_factory=async_sessionmaker(
-            autocommit=False,
-            autoflush=False,
-            bind=pg_engine_async_null_pool,
-            expire_on_commit=False,
-        ),
-        scopefunc=asyncio.current_task,
-    )
-    _SessionReadOnlyAsync = async_scoped_session(
-        async_sessionmaker(
-            autocommit=False,
-            autoflush=False,
-            bind=pg_engine_read_only_async,
-            expire_on_commit=False,
-        ),
-        scopefunc=asyncio.current_task,
-    )
+@dataclass
+class AsyncDBSession:
+    engine: AsyncEngine
+    session: async_scoped_session
 
 
-refresh_sessions()
+@dataclass
+class DBSessions:
+    session_sync: SyncDBSession
+    session_sync_ro: SyncDBSession
+    session_async: AsyncDBSession
+    session_async_ro: AsyncDBSession
+    session_async_null_pool: AsyncDBSession
+
+
+@dataclass
+class DBEngineConfig:
+    pool_pre_ping: bool
+    pool_size: int
+    max_overflow: int
+    echo: bool
+    echo_pool: bool
+
+
+class DBManager:
+    sessions: DBSessions
+    config: DBEngineConfig
+
+    credential_expiration_timestamp: Optional[float] = None
+    credential_expiration_buffer_sec: int = 300
+
+    def __init__(self, infra_config: InfraConfig):
+        self.pool_pre_ping = infra_config.db_engine_disconnect_strategy == "pessimistic"
+        self.pool_size = infra_config.db_engine_pool_size
+        self.max_overflow = infra_config.db_engine_max_overflow
+        self.echo = infra_config.db_engine_echo
+        self.echo_pool = infra_config.db_engine_echo_pool
+        self.sessions = self.refresh_sessions()
+
+    def refresh_sessions(self) -> DBSessions:
+        self.sessions.session_sync.session.close_all()
+
+        db_connection = get_engine_url(read_only=False, sync=True)
+        # use sync engine as proxy for credential expiration
+        self.credential_expiration_timestamp = db_connection.expiry_in_sec
+        pg_engine = create_engine(
+            db_connection.url,
+            echo=self.echo,
+            echo_pool=self.echo_pool,
+            pool_pre_ping=self.pool_pre_ping,
+            pool_size=self.pool_size,
+            max_overflow=self.max_overflow,
+            future=True,
+            logging_name="sync",
+        )
+        session_sync = SyncDBSession(
+            engine=pg_engine,
+            session=sessionmaker(autocommit=False, autoflush=False, bind=pg_engine),
+        )
+
+        pg_engine_ro = create_engine(
+            url=get_engine_url(read_only=True, sync=True).url,
+            echo=self.echo,
+            echo_pool=self.echo_pool,
+            pool_pre_ping=self.pool_pre_ping,
+            pool_size=self.pool_size,
+            max_overflow=self.max_overflow,
+            future=True,
+            logging_name="sync_ro",
+        )
+        session_sync_ro = sessionmaker(autocommit=False, autoflush=False, bind=pg_engine_ro)
+
+        pg_engine_async = create_async_engine(
+            url=get_engine_url(read_only=False, sync=False).url,
+            echo=self.echo,
+            echo_pool=self.echo_pool,
+            pool_pre_ping=self.pool_pre_ping,
+            pool_size=self.pool_size,
+            max_overflow=self.max_overflow,
+            future=True,
+            logging_name="async",
+        )
+        session_async = async_scoped_session(
+            session_factory=async_sessionmaker(
+                autocommit=False,
+                autoflush=False,
+                bind=pg_engine_async,
+                expire_on_commit=False,
+            ),
+            scopefunc=asyncio.current_task,
+        )
+
+        pg_engine_async_ro = create_async_engine(
+            url=get_engine_url(read_only=True, sync=False).url,
+            echo=self.echo,
+            echo_pool=self.echo_pool,
+            pool_pre_ping=self.pool_pre_ping,
+            pool_size=self.pool_size,
+            max_overflow=self.max_overflow,
+            future=True,
+            logging_name="async_ro",
+        )
+        session_async_ro = async_scoped_session(
+            async_sessionmaker(
+                autocommit=False,
+                autoflush=False,
+                bind=pg_engine_async_ro,
+                expire_on_commit=False,
+            ),
+            scopefunc=asyncio.current_task,
+        )
+
+        pg_engine_async_null_pool = create_async_engine(
+            url=get_engine_url(read_only=False, sync=False).url,
+            echo=self.echo,
+            echo_pool=self.echo_pool,
+            future=True,
+            poolclass=NullPool,
+            logging_name="async_null",
+        )
+
+        session_async_null_pool = async_scoped_session(
+            session_factory=async_sessionmaker(
+                autocommit=False,
+                autoflush=False,
+                bind=pg_engine_async_null_pool,
+                expire_on_commit=False,
+            ),
+            scopefunc=asyncio.current_task,
+        )
+
+        return DBSessions(
+            session_sync=session_sync,
+            session_sync_ro=session_sync_ro,
+            session_async=session_async,
+            session_async_ro=session_async_ro,
+            session_async_null_pool=session_async_null_pool,
+        )
+
+    def _is_credentials_expired(self):
+        return (
+            self.credential_expiration_timestamp is not None
+            and time.time()
+            > self.credential_expiration_timestamp - self.credential_expiration_buffer_sec
+        )
+
+    def get_session_sync(self):
+        if self._is_credentials_expired():
+            self.refresh_sessions()
+        return self.sessions.session_sync.session
+
+    def get_session_sync_ro(self):
+        if self._is_credentials_expired():
+            self.refresh_sessions()
+        return self.sessions.session_sync_ro.session
+
+    def get_session_async(self):
+        if self._is_credentials_expired():
+            self.refresh_sessions()
+        return self.sessions.session_async.session
+
+    def get_session_async_ro(self):
+        if self._is_credentials_expired():
+            self.refresh_sessions()
+        return self.sessions.session_async_ro.session
+
+    def get_session_async_null_pool(self):
+        if self._is_credentials_expired():
+            self.refresh_sessions()
+        return self.sessions.session_async_null_pool.session
+
+
+db_manager = DBManager(infra_config())
 
 
 def get_session():
-    global _Session
-    global database_credential_expiration_timestamp
-
-    if time.time() > database_credential_expiration_timestamp - EXPIRATION_BUFFER:
-        refresh_sessions()
-
-    return _Session
+    return db_manager.get_session_sync()
 
 
 def get_session_read_only():
-    global _SessionReadOnly
-    global database_credential_expiration_timestamp
-
-    if time.time() > database_credential_expiration_timestamp - EXPIRATION_BUFFER:
-        refresh_sessions()
-
-    return _SessionReadOnly
+    return db_manager.get_session_sync_ro()
 
 
 def get_session_async():
-    global _SessionAsync
-    global database_credential_expiration_timestamp
-
-    if time.time() > database_credential_expiration_timestamp - EXPIRATION_BUFFER:
-        refresh_sessions()
-
-    return _SessionAsync
+    return db_manager.get_session_async()
 
 
 def get_session_async_null_pool():
-    global _SessionAsyncNullPool
-    global database_credential_expiration_timestamp
-
-    if time.time() > database_credential_expiration_timestamp - EXPIRATION_BUFFER:
-        refresh_sessions()
-
-    return _SessionAsyncNullPool
+    return db_manager.get_session_async_null_pool()
 
 
 def get_session_read_only_async():
-    global _SessionReadOnlyAsync
-    global database_credential_expiration_timestamp
-
-    if time.time() > database_credential_expiration_timestamp - EXPIRATION_BUFFER:
-        refresh_sessions()
-
-    return _SessionReadOnlyAsync
+    return db_manager.get_session_async_ro()
 
 
 Base = declarative_base()
