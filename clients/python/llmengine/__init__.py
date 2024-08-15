51220bf6e4670e95a0577f870a1666e35449927e
Michael Choi <michael.choi@scale.com>
2024-08-15 18:16:45 +0000

    Bump version

--- clients/python/llmengine/__init__.py
+++ clients/python/llmengine/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.0.0beta35"
+__version__ = "0.0.0beta36"
 
 import os
 from typing import Sequence
