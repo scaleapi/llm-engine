fc6d55d4239ea7e504907c20852bc833afb628d6
Michael Choi <michael.choi@scale.com>
2024-08-20 21:54:01 +0000

    Bump client min python

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="scale-llm-engine",
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     version="0.0.0.beta37",
     packages=find_packages(),
     package_data={"llmengine": ["py.typed"]},
