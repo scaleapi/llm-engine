439e001fe8011c0292d55ef4a009e6f3bcb0c621
Michael Choi <michael2choi@gmail.com>
2024-08-21 14:17:57 -0700

    Batch completions V2 job (#602)
    * Batch V2 - v1 payload working
    
    * Fix types
    
    * Add more examples
    
    * Update sdk
    
    * fix package versions for docs
    
    * more updates
    
    * Batch completions V2 API
    
    * Fix test
    
    * Bump client min python
    
    * Add v2 router to app
    
    * Add v2 router to app
    
    * Some fixes
    
    * Some fixes
    
    * Update client

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="scale-llm-engine",
-    python_requires=">=3.7",
-    version="0.0.0.beta36",
+    python_requires=">=3.8",
+    version="0.0.0.beta37",
     packages=find_packages(),
     package_data={"llmengine": ["py.typed"]},
 )
