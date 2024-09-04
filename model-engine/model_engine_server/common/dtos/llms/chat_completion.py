fb3776d505fac8ca9b0efb7d4c59b03300a641b9
Michael Choi <michael.choi@scale.com>
2024-09-04 00:15:12 +0000

    separate vllm params

--- model-engine/model_engine_server/common/dtos/llms/chat_completion.py
+++ model-engine/model_engine_server/common/dtos/llms/chat_completion.py
@@ -1,104 +1,19 @@
-from typing import Any, Dict, List, Optional
+from typing import Optional
 
+from model_engine_server.common.dtos.llms.vllm import VLLMChatCompletionAdditionalParams
+from model_engine_server.common.pydantic_types import Field
 from model_engine_server.common.types.gen.openai import (
     CreateChatCompletionRequest,
     CreateChatCompletionResponse,
+    CreateChatCompletionStreamResponse,
 )
-from pydantic import Field
 from typing_extensions import Annotated
 
 # Fields that are a part of OpenAI spec but are not supported by model engine
 UNSUPPORTED_FIELDS = ["service_tier"]
 
 
-class VLLMAdditionalFields:
-    chat_template: Annotated[
-        Optional[str],
-        Field(
-            default=None,
-            description=(
-                "A Jinja template to use for this conversion. "
-                "As of transformers v4.44, default chat template is no longer "
-                "allowed, so you must provide a chat template if the tokenizer "
-                "does not define one."
-            ),
-        ),
-    ]
-    chat_template_kwargs: Annotated[
-        Optional[Dict[str, Any]],
-        Field(
-            default=None,
-            description=(
-                "Additional kwargs to pass to the template renderer. "
-                "Will be accessible by the chat template."
-            ),
-        ),
-    ]
-
-    guided_json: Annotated[
-        Optional[Dict[str, Any]],
-        Field(
-            default=None,
-            description="JSON schema for guided decoding. Only supported in vllm.",
-        ),
-    ]
-
-    guided_regex: Annotated[
-        Optional[str],
-        Field(
-            default=None,
-            description="Regex for guided decoding. Only supported in vllm.",
-        ),
-    ]
-    guided_choice: Annotated[
-        Optional[List[str]],
-        Field(
-            default=None,
-            description="Choices for guided decoding. Only supported in vllm.",
-        ),
-    ]
-
-    guided_grammar: Annotated[
-        Optional[str],
-        Field(
-            default=None,
-            description="Context-free grammar for guided decoding. Only supported in vllm.",
-        ),
-    ]
-
-    guided_decoding_backend: Annotated[
-        Optional[str],
-        Field(
-            default=None,
-            description=(
-                "If specified, will override the default guided decoding backend "
-                "of the server for this specific request. If set, must be either "
-                "'outlines' / 'lm-format-enforcer'"
-            ),
-        ),
-    ]
-
-    guided_whitespace_pattern: Annotated[
-        Optional[str],
-        Field(
-            default=None,
-            description=(
-                "If specified, will override the default whitespace pattern "
-                "for guided json decoding."
-            ),
-        ),
-    ]
-
-    skip_special_tokens: Annotated[
-        Optional[bool],
-        Field(
-            True,
-            description="Whether to skip special tokens in the output. Only supported in vllm.",
-        ),
-    ]
-
-
-class ChatCompletionV2Request(CreateChatCompletionRequest, VLLMAdditionalFields):
+class ChatCompletionV2Request(CreateChatCompletionRequest, VLLMChatCompletionAdditionalParams):
     model: Annotated[
         str,
         Field(
@@ -115,20 +30,10 @@ class ChatCompletionV2Request(CreateChatCompletionRequest, VLLMAdditionalFields)
         ),
     ]
 
-    top_k: Annotated[
-        Optional[int],
-        Field(
-            None,
-            ge=-1,
-            description="Controls the number of top tokens to consider. -1 means consider all tokens.",
-        ),
-    ]
-
-    include_stop_str_in_output: Annotated[
-        Optional[bool],
-        Field(None, description="Whether to include the stop strings in output text."),
-    ]
-
 
 class ChatCompletionV2Response(CreateChatCompletionResponse):
+    pass
+
+
+class ChatCompletionV2Chunk(CreateChatCompletionStreamResponse):
     pass
