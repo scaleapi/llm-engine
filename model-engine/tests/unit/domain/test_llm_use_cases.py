354e40fa43bbded903ab568a07c2fb87c2c6dfa5
Michael Choi <michael.choi@scale.com>
2024-09-10 18:52:45 +0000

    Try to get helm charts working on circleci

--- model-engine/tests/unit/domain/test_llm_use_cases.py
+++ model-engine/tests/unit/domain/test_llm_use_cases.py
@@ -446,7 +446,7 @@ def test_load_model_weights_sub_commands(
 
     expected_result = [
         "s5cmd > /dev/null || conda install -c conda-forge -y s5cmd",
-        './s5cmd --numworkers 512 cp --concurrency 10 --include "*.model" --include "*.json" --include "*.safetensors" --exclude "optimizer*" s3://fake-checkpoint/* test_folder',
+        's5cmd --numworkers 512 cp --concurrency 10 --include "*.model" --include "*.json" --include "*.safetensors" --exclude "optimizer*" s3://fake-checkpoint/* test_folder',
     ]
     assert expected_result == subcommands
 
