79dba55cc0fcc315b7f679fda1553f40c5a4c481
Michael Choi <michael2choi@gmail.com>
2024-09-12 12:03:30 -0700

    Fix passing in vllm args options (#611)

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="scale-llm-engine",
     python_requires=">=3.8",
-    version="0.0.0.beta38",
+    version="0.0.0.beta39",
     packages=find_packages(),
     package_data={"llmengine": ["py.typed"]},
 )
