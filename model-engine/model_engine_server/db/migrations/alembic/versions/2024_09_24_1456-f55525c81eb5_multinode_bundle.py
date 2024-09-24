b9b4ff387f42d8bd80f6b764793c0c75236fa59e
Sean Shi <sean.shi@scale.com>
2024-09-24 14:57:27 -0700

    add multinode bundle file

---
+++ model-engine/model_engine_server/db/migrations/alembic/versions/2024_09_24_1456-f55525c81eb5_multinode_bundle.py
@@ -0,0 +1,25 @@
+"""multinode_bundle
+
+Revision ID: f55525c81eb5
+Revises: b574e9711e35
+Create Date: 2024-09-24 14:56:36.287001
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "f55525c81eb5"
+down_revision = "b574e9711e35"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    # TODO
+    pass
+
+
+def downgrade() -> None:
+    # TODO
+    pass
