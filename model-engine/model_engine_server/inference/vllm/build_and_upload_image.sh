696c94fcf5143ea61052874703f96fa78eb28b80
Michael Choi <michael2choi@gmail.com>
2024-10-14 15:29:25 -0700

    vllm 0.6.3 (#636)

--- model-engine/model_engine_server/inference/vllm/build_and_upload_image.sh
+++ model-engine/model_engine_server/inference/vllm/build_and_upload_image.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 # Build and push vLLM docker image to AWS ECR.
 #
-# Usage: VLLM_VERSION=0.5.3.post1 ./build_and_upload_image.sh <AWS_ACCOUNT_ID> <IMAGE_TAG> vllm|vllm_batch|vllm_batch_v2
+# Usage: VLLM_VERSION=0.6.3 ./build_and_upload_image.sh <AWS_ACCOUNT_ID> <IMAGE_TAG> vllm|vllm_batch|vllm_batch_v2
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR=$SCRIPT_DIR/../../../..
@@ -21,14 +21,16 @@ if [ -z "$2" ]; then
 fi
 
 if [ -z "$3" ]; then
-  echo "Must supply the build target (either vllm or vllm_batch)"
+  echo "Must supply the build target (either vllm or vllm_batch_v2)"
   exit 1;
 fi
+
 
 ACCOUNT=$1
 IMAGE_TAG=$2
 BUILD_TARGET=$3
 VLLM_VERSION=${VLLM_VERSION:-"0.6.2"}
+VLLM_BASE_REPO=${VLLM_BASE_REPO:-"vllm/vllm-openai"}
 
 # if build target = vllm use vllm otherwise use vllm_batch
 if [ "$BUILD_TARGET" == "vllm" ]; then
@@ -40,6 +42,7 @@ fi
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ACCOUNT.dkr.ecr.us-west-2.amazonaws.com
 DOCKER_BUILDKIT=1 docker build \
   --build-arg VLLM_VERSION=${VLLM_VERSION} \
+  --build-arg VLLM_BASE_REPO=${VLLM_BASE_REPO} \
   -f Dockerfile.vllm \
   --target ${BUILD_TARGET} \
   -t $IMAGE ${PROJECT_DIR}
