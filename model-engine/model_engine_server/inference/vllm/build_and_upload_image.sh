1437dbfd8fce5e0af685205c99f20439adc4b161
Michael Choi <michael2choi@gmail.com>
2024-11-21 20:53:20 -0800

    Support pixtral large (#663)
    * Support pixtral large
    
    * fix test

--- model-engine/model_engine_server/inference/vllm/build_and_upload_image.sh
+++ model-engine/model_engine_server/inference/vllm/build_and_upload_image.sh
@@ -4,11 +4,11 @@ set -eo pipefail
 
 # Build and push vLLM docker image to AWS ECR.
 #
-# Usage: VLLM_VERSION=0.6.3 ./build_and_upload_image.sh <AWS_ACCOUNT_ID> <IMAGE_TAG> vllm|vllm_batch|vllm_batch_v2
+# Usage: VLLM_VERSION=0.6.4.post1 ./build_and_upload_image.sh <AWS_ACCOUNT_ID> <IMAGE_TAG> vllm|vllm_batch|vllm_batch_v2
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR=$SCRIPT_DIR/../../../..
-DOCKERFILE=$PROJECT_DIR/model_engine_server/inference/vllm/Dockerfile.vllm
+DOCKERFILE=$PROJECT_DIR/model-engine/model_engine_server/inference/vllm/Dockerfile.vllm
 
 if [ -z "$1" ]; then
   echo "Must supply AWS account ID"
@@ -29,7 +29,7 @@ fi
 ACCOUNT=$1
 IMAGE_TAG=$2
 BUILD_TARGET=$3
-VLLM_VERSION=${VLLM_VERSION:-"0.6.2"}
+VLLM_VERSION=${VLLM_VERSION:-"0.6.4.post1"}
 VLLM_BASE_REPO=${VLLM_BASE_REPO:-"vllm/vllm-openai"}
 
 # if build target = vllm use vllm otherwise use vllm_batch
