e93b04cf381e38c71c69efb2feb22d2155dbee0d
Sean Shi <sean.shi@scale.com>
2024-12-12 14:31:36 -0800

    missed a spot

--- model-engine/model_engine_server/infra/gateways/live_model_endpoint_infra_gateway.py
+++ model-engine/model_engine_server/infra/gateways/live_model_endpoint_infra_gateway.py
@@ -146,6 +146,9 @@ class LiveModelEndpointInfraGateway(ModelEndpointInfraGateway):
             max_workers = infra_state.deployment_state.max_workers
         if per_worker is None:
             per_worker = infra_state.deployment_state.per_worker
+        if concurrent_requests is None:
+            concurrent_requests = 1  # obviously change this
+            # concurrent_requests = infra_state.deployment_state.concurrent_requests
         if cpus is None:
             cpus = infra_state.resource_state.cpus
         if gpus is None:
@@ -201,7 +204,7 @@ class LiveModelEndpointInfraGateway(ModelEndpointInfraGateway):
             min_workers=min_workers,
             max_workers=max_workers,
             per_worker=per_worker,
-            concurrent_requests=1,  # TODO
+            concurrent_requests=concurrent_requests,
             cpus=cpus,
             gpus=gpus,
             memory=memory,
