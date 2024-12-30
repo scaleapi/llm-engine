e55047b3afe0afc8c9a01b83468d16b14964ad61
Sean Shi <sean.shi@scale.com>
2024-12-30 10:12:21 -0800

    temp comment out download to speed up iterations

--- model-engine/model_engine_server/inference/vllm/vllm_batch.py
+++ model-engine/model_engine_server/inference/vllm/vllm_batch.py
@@ -333,11 +333,13 @@ async def handle_batch_job(
 
     model = get_model_name(request.model_cfg)
     if request.model_cfg.checkpoint_path:
-        await download_model(
-            checkpoint_path=request.model_cfg.checkpoint_path,
-            target_dir=MODEL_WEIGHTS_FOLDER,
-            trust_remote_code=request.model_cfg.trust_remote_code or False,
-        )
+        pass
+        # TODO change back
+        # await download_model(
+        #     checkpoint_path=request.model_cfg.checkpoint_path,
+        #     target_dir=MODEL_WEIGHTS_FOLDER,
+        #     trust_remote_code=request.model_cfg.trust_remote_code or False,
+        # )
 
     # TODO init the ray cluster here
 
