d51f0f4957b6c9352f0e5f6b3c96ac06e2f45b90
Josh Clemons <josh.clemons@scale.com>
2025-09-04 03:00:26 +0000

    checkpt

--- model-engine/model_engine_server/inference/vllm/vllm_server.py
+++ model-engine/model_engine_server/inference/vllm/vllm_server.py
@@ -17,13 +17,16 @@ from vllm.engine.async_llm_engine import (
 )
 from vllm.engine.protocol import EngineClient
 from vllm.entrypoints.launcher import serve_http
-from vllm.entrypoints.openai.api_server import build_app, build_async_engine_client, init_app_state
+from vllm.entrypoints.openai.api_server import build_app, build_async_engine_client, init_app_state, run_server, load_log_config, maybe_register_tokenizer_info_endpoint
 from vllm.entrypoints.openai.cli_args import make_arg_parser
 from vllm.outputs import CompletionOutput
 from vllm.sampling_params import SamplingParams
 from vllm.sequence import Logprob
-from vllm.utils import FlexibleArgumentParser, random_uuid
+from vllm.utils import FlexibleArgumentParser, random_uuid, is_valid_ipv6_address, set_ulimit
 from vllm.version import __version__ as VLLM_VERSION
+
+from vllm.entrypoints.openai.tool_parsers import ToolParserManager
+
 
 logger = Logger("vllm_server")
 
@@ -195,36 +198,102 @@ def parse_args(parser: FlexibleArgumentParser):
     parser.add_argument("--attention-backend", type=str, help="The attention backend to use")
     return parser.parse_args()
 
+def create_server_socket(addr: tuple[str, int]) -> socket.socket:
+    family = socket.AF_INET
+    if is_valid_ipv6_address(addr[0]):
+        family = socket.AF_INET6
 
-async def run_server(args, **uvicorn_kwargs) -> None:
-    logger.info("vLLM API server version %s", VLLM_VERSION)
-    logger.info("args: %s", args)
+    sock = socket.socket(family=family, type=socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    sock.bind(addr)
 
-    temp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # nosemgrep
-    temp_socket.bind(("", args.port))
+    return sock
 
-    def signal_handler(*_) -> None:
-        # Interrupt server on sigterm while initializing
-        raise KeyboardInterrupt("terminated")
+# async def run_server(args, **uvicorn_kwargs) -> None:
+#     logger.info("vLLM API server version %s", VLLM_VERSION)
+#     logger.info("args: %s", args)
+    
+#     validate_api_server_args(args)
 
-    signal.signal(signal.SIGTERM, signal_handler)
+#     sock_addr = (args.host or "", args.port)
+#     sock = create_server_socket(sock_addr)
+
+#     def signal_handler(*_) -> None:
+#         # Interrupt server on sigterm while initializing
+#         raise KeyboardInterrupt("terminated")
+
+#     signal.signal(signal.SIGTERM, signal_handler)
+
+#     global engine_client
+#     async with build_async_engine_client(args) as engine_client:
+#         app = build_app(args)
+
+#         # model_config = await engine_client.get_model_config()
+#         vllm_config = await engine_client.get_vllm_config()
+#         await init_app_state(engine_client, vllm_config, app.state, args)
+
+#         temp_socket.close()
+#         app.include_router(router)
+
+#         shutdown_task = await serve_http(
+#             app,
+#             None,
+#             host=args.host,
+#             port=args.port,
+#             log_level=args.uvicorn_log_level,
+#             timeout_keep_alive=TIMEOUT_KEEP_ALIVE,
+#             ssl_keyfile=args.ssl_keyfile,
+#             ssl_certfile=args.ssl_certfile,
+#             ssl_ca_certs=args.ssl_ca_certs,
+#             ssl_cert_reqs=args.ssl_cert_reqs,
+#             **uvicorn_kwargs,
+#         )
+
+#     # NB: Await server shutdown only after the backend context is exited
+#     await shutdown_task
+
+
+async def run_server_worker(listen_address,
+                            sock,
+                            args,
+                            client_config=None,
+                            **uvicorn_kwargs) -> None:
+    """Run a single API server worker."""
+
+    if args.tool_parser_plugin and len(args.tool_parser_plugin) > 3:
+        ToolParserManager.import_tool_parser(args.tool_parser_plugin)
+
+    server_index = client_config.get("client_index", 0) if client_config else 0
+
+    # Load logging config for uvicorn if specified
+    log_config = load_log_config(args.log_config_file)
+    if log_config is not None:
+        uvicorn_kwargs['log_config'] = log_config
 
     global engine_client
-    async with build_async_engine_client(args) as engine_client:
+
+    async with build_async_engine_client(args, client_config) as engine_client:
+        maybe_register_tokenizer_info_endpoint(args)
         app = build_app(args)
 
-        model_config = await engine_client.get_model_config()
-        await init_app_state(engine_client, model_config, app.state, args)
-
-        temp_socket.close()
+        vllm_config = await engine_client.get_vllm_config()
+        await init_app_state(engine_client, vllm_config, app.state, args)
         app.include_router(router)
 
+        logger.info("Starting vLLM API server %d on %s", server_index,
+                    listen_address)
         shutdown_task = await serve_http(
             app,
+            sock=sock,
+            enable_ssl_refresh=args.enable_ssl_refresh,
             host=args.host,
             port=args.port,
             log_level=args.uvicorn_log_level,
-            timeout_keep_alive=TIMEOUT_KEEP_ALIVE,
+            # NOTE: When the 'disable_uvicorn_access_log' value is True,
+            # no access log will be output.
+            access_log=not args.disable_uvicorn_access_log,
+            timeout_keep_alive=envs.VLLM_HTTP_TIMEOUT_KEEP_ALIVE,
             ssl_keyfile=args.ssl_keyfile,
             ssl_certfile=args.ssl_certfile,
             ssl_ca_certs=args.ssl_ca_certs,
@@ -233,7 +302,10 @@ async def run_server(args, **uvicorn_kwargs) -> None:
         )
 
     # NB: Await server shutdown only after the backend context is exited
-    await shutdown_task
+    try:
+        await shutdown_task
+    finally:
+        sock.close()
 
 
 if __name__ == "__main__":
