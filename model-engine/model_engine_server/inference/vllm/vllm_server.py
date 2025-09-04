c49fc137cea690e1083ac69e96383568997855d3
Josh Clemons <josh.clemons@scale.com>
2025-09-04 03:15:30 +0000

    checkpt

--- model-engine/model_engine_server/inference/vllm/vllm_server.py
+++ model-engine/model_engine_server/inference/vllm/vllm_server.py
@@ -17,16 +17,14 @@ from vllm.engine.async_llm_engine import (
 )
 from vllm.engine.protocol import EngineClient
 from vllm.entrypoints.launcher import serve_http
-from vllm.entrypoints.openai.api_server import build_app, build_async_engine_client, init_app_state, run_server, load_log_config, maybe_register_tokenizer_info_endpoint
+from vllm.entrypoints.openai.api_server import build_app, build_async_engine_client, create_server_socket, init_app_state, load_log_config, maybe_register_tokenizer_info_endpoint, run_server
 from vllm.entrypoints.openai.cli_args import make_arg_parser
+from vllm.entrypoints.openai.tool_parsers import ToolParserManager
 from vllm.outputs import CompletionOutput
 from vllm.sampling_params import SamplingParams
 from vllm.sequence import Logprob
-from vllm.utils import FlexibleArgumentParser, random_uuid, is_valid_ipv6_address, set_ulimit
+from vllm.utils import FlexibleArgumentParser, is_valid_ipv6_address, random_uuid, set_ulimit
 from vllm.version import __version__ as VLLM_VERSION
-
-from vllm.entrypoints.openai.tool_parsers import ToolParserManager
-
 
 logger = Logger("vllm_server")
 
@@ -197,62 +195,6 @@ def parse_args(parser: FlexibleArgumentParser):
     parser = make_arg_parser(parser)
     parser.add_argument("--attention-backend", type=str, help="The attention backend to use")
     return parser.parse_args()
-
-def create_server_socket(addr: tuple[str, int]) -> socket.socket:
-    family = socket.AF_INET
-    if is_valid_ipv6_address(addr[0]):
-        family = socket.AF_INET6
-
-    sock = socket.socket(family=family, type=socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    sock.bind(addr)
-
-    return sock
-
-# async def run_server(args, **uvicorn_kwargs) -> None:
-#     logger.info("vLLM API server version %s", VLLM_VERSION)
-#     logger.info("args: %s", args)
-    
-#     validate_api_server_args(args)
-
-#     sock_addr = (args.host or "", args.port)
-#     sock = create_server_socket(sock_addr)
-
-#     def signal_handler(*_) -> None:
-#         # Interrupt server on sigterm while initializing
-#         raise KeyboardInterrupt("terminated")
-
-#     signal.signal(signal.SIGTERM, signal_handler)
-
-#     global engine_client
-#     async with build_async_engine_client(args) as engine_client:
-#         app = build_app(args)
-
-#         # model_config = await engine_client.get_model_config()
-#         vllm_config = await engine_client.get_vllm_config()
-#         await init_app_state(engine_client, vllm_config, app.state, args)
-
-#         temp_socket.close()
-#         app.include_router(router)
-
-#         shutdown_task = await serve_http(
-#             app,
-#             None,
-#             host=args.host,
-#             port=args.port,
-#             log_level=args.uvicorn_log_level,
-#             timeout_keep_alive=TIMEOUT_KEEP_ALIVE,
-#             ssl_keyfile=args.ssl_keyfile,
-#             ssl_certfile=args.ssl_certfile,
-#             ssl_ca_certs=args.ssl_ca_certs,
-#             ssl_cert_reqs=args.ssl_cert_reqs,
-#             **uvicorn_kwargs,
-#         )
-
-#     # NB: Await server shutdown only after the backend context is exited
-#     await shutdown_task
-
 
 async def run_server_worker(listen_address,
                             sock,
