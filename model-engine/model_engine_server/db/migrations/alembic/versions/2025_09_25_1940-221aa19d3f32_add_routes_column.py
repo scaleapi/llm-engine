5d8a634d502744b2a55a0853280179a4f4f827b7
meher-m <meher.mankikar@scale.com>
2025-09-25 19:42:07 +0000

    add file for db model change

---
+++ model-engine/model_engine_server/db/migrations/alembic/versions/2025_09_25_1940-221aa19d3f32_add_routes_column.py
@@ -0,0 +1,32 @@
+"""add routes column
+
+Revision ID: 221aa19d3f32
+Revises: e580182d6bfd
+Create Date: 2025-09-25 19:40:24.927198
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '221aa19d3f32'
+down_revision = 'e580182d6bfd'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        'bundles',
+        sa.Column('routes', sa.ARRAY(sa.Text), nullable=True),
+        schema='hosted_model_inference',
+    )
+
+
+def downgrade() -> None:
+    op.drop_column(
+        'bundles',
+        'routes',
+        schema='hosted_model_inference',
+    )
